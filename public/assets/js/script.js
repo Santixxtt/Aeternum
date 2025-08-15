84b6598ca959342e2cae643f606ffb6d5c06e897
Santixxtt <tutadavid1@gmail.com>
2025-08-14 22:11:07 -0500

    Pagina Aeternum

--- public/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function () {
-    const navbar = document.getElementById('navbar');
-    if (window.scrollY > 50) {
-        navbar.classList.add('sticky');
-    } else {
-        navbar.classList.remove('sticky');
-    }
-});
